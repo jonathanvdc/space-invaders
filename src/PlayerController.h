2fb82b8a389a25c117c51e456451eacf11b77381
jonathanvdc <jonathan.vdc@outlook.com>
2015-11-25 11:54:56 +0100

    Player controller handles input now

--- src/PlayerController.h
+++ src/PlayerController.h
@@ -9,15 +9,22 @@ namespace si
 	namespace controller
 	{
 		/// A controller class for the player's ship.
+		/// Player controllers convert keyboard input
+		/// into changes to the game world.
 		class PlayerController final : public IController
 		{
 		public:
-			PlayerController(std::shared_ptr<si::model::ShipEntity> player);
+			/// Creates a player controller from the given player and
+			/// acceleration multiplier.
+			PlayerController(std::shared_ptr<si::model::ShipEntity> player, double acceleration);
 
 			/// Updates the game model based on the given time delta.
 			void update(si::model::Game& game, duration_t timeDelta) const final override;
 		private:
+			/// The player entity.
 			std::shared_ptr<si::model::ShipEntity> player;
+			/// An acceleration multiplier.
+			double accelConst;
 		};
 	}
 }
