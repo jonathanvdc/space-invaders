15e10e14926636993899e17897c7866228465e3a
jonathanvdc <jonathan.vdc@outlook.com>
2016-01-08 15:13:49 +0100

    Created a run.sh shell script

---
+++ run.sh
@@ -0,0 +1,19 @@
+# This script builds space-invaders, and then runs it on the scene with the
+# given name. Any `*.xml` file in the `scenes/` directory can be used
+# for this puropose.
+
+if test "1" -eq "$#"; then
+    cd src
+    mkdir build -p
+    cd build
+    cmake ..
+    make -j2
+    cd ../../scenes
+    ../src/build/space-invaders $@
+    cd ..
+    echo "Thanks for playing!"
+else
+    echo "Expected exactly one input argument. Sorry :/"
+    echo "This script builds space-invaders, and then runs it on the scene with the given name. Any '.xml' file in the 'scenes/' directory can be used for this purpose."
+    echo "For example, you could give '$0 namespace-invaders.xml' a try."
+fi
